bb0a1ef19998d9b6f80d7f58f202c25f511c4dd3
jay <jangdekaran4@gmail.com>
2024-08-30 17:06:07 +0530

    update

--- web/next.config.js
+++ web/next.config.js
@@ -7,9 +7,6 @@ const nextConfig = {
     minimumCacheTTL : 60 * 60 * 24,
     deviceSizes: [180, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840]
   },
-  eslint:{
-    ignoreDuringBuilds:true
-  },
 
   productionBrowserSourceMaps: true,
   trailingSlash : true
