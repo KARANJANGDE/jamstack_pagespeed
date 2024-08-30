993f324ceb290e08f450d8a0135b4ee986b58040
jay <jangdekaran4@gmail.com>
2024-08-30 16:54:38 +0530

    error changed

--- web/next.config.js
+++ web/next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
     minimumCacheTTL : 60 * 60 * 24,
     deviceSizes: [180, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840]
   },
+  eslint:{
+    ignoreDuringBuilds:true
+  },
 
   productionBrowserSourceMaps: true,
   trailingSlash : true
